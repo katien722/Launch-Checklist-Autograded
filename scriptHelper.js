8932af878f8bbf6be7e3eb1b8df544a2c0b925b4
katien722 <katien722@gmail.com>
2024-05-10 10:54:47 -0500

    GA5 FINAL COMMIT, demo'd and all

--- scriptHelper.js
+++ scriptHelper.js
@@ -5,18 +5,15 @@ require('cross-fetch/polyfill');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
 
-//see pic of Carrie's review on Thursday with 'data.forEach(planet => { ...will we use this?
-
-
     
     document.getElementById("missionTarget").innerHTML = `
                  <h2>Mission Destination</h2>
                  <ol>
-                     <li>name: ${name} </li> 
-                     <li>diameter: ${diameter} </li>
-                     <li>star: ${star}</li>
-                     <li>distance: ${distance}</li>
-                     <li>moons: ${moons} </li>
+                     <li>Name: ${name} </li> 
+                     <li>Diameter: ${diameter} </li>
+                     <li>Star: ${star}</li>
+                     <li>Distance from Earth: ${distance}</li>
+                     <li>Number of Moons: ${moons} </li>
                  </ol>
                  <img src="${imageUrl}">`
     
@@ -36,6 +33,11 @@ function validateInput(testInput) {
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 
+    const pilotStatus = document.getElementById("pilotStatus")
+    const copilotStatus = document.getElementById("copilotStatus")
+    const fuelStatus = document.getElementById("fuelStatus")
+    const cargoStatus = document.getElementById("cargoStatus")
+    const launchStatus = document.getElementById("launchStatus")
     if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
 
 
@@ -46,11 +48,6 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         window.alert('Fuel level and Cargo levels must be numbers')
     } else {
         list.style.visibility = "visible"
-        const pilotStatus = document.getElementById("pilotStatus")
-        const copilotStatus = document.getElementById("copilotStatus")
-        const fuelStatus = document.getElementById("fuelStatus")
-        const cargoStatus = document.getElementById("cargoStatus")
-        const launchStatus = document.getElementById("launchStatus")
 
         pilotStatus.innerHTML = (`Pilot ${pilot} is ready for launch`)
         copilotStatus.innerHTML = (`Co-pilot ${copilot} is ready for launch`)
@@ -65,13 +62,14 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         } else {
             cargoStatus.innerHTML = ('Cargo mass low enough for launch')
         }
-        if (cargoLevel < 10000 && fuelLevel > 10000) {
+        if (cargoLevel <= 10000 && fuelLevel >= 10000) {
             launchStatus.innerHTML = (`Shuttle is Ready for Launch`)
         } else {
             launchStatus.innerHTML = (`Shuttle Not Ready for Launch`)
         }
 
     }
+    console.log(launchStatus.innerHTML)
     if (launchStatus.innerHTML === 'Shuttle is Ready for Launch') {
         launchStatus.style.color = "green"
     } else if (launchStatus.innerHTML === 'Shuttle Not Ready for Launch') {
@@ -88,9 +86,9 @@ async function myFetch() {
 function pickPlanet(planets) {
   return planets[(Math.floor(Math.random() * planets.length))];
 }
-console.log(pickPlanet(planets))
 
-module.exports.addDestinationInfo = paddDestinationInfo;
+
+module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
 module.exports.pickPlanet = pickPlanet;
